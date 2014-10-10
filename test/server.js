4f46b86b754c8576dc603c1da84c31ce4a78eab5
Vincent Terrasi <vterrasi@oxygem.tv>
2014-10-10 12:32:30 +0200

    init

---
+++ test/server.js
@@ -0,0 +1,38 @@
+var seneca = require('seneca')()
+
+seneca.add('role:v1,cmd:caz',function(args,done){
+  done(null,{caz:args.q+' ANALYSE '})
+})
+
+
+seneca.add('role:foo,cmd:bar',function(args,done){
+  done(null,{bar:args.zoo+'b'})
+})
+
+seneca.add('role:foo,cmd:qaz',function(args,done){
+  done(null,{qaz:args.zoo+'z'})
+})
+
+
+seneca.act('role:web',{use:{
+  prefix:'/foo',
+  pin:{role:'foo',cmd:'*'},
+  map:{
+    zig: true,
+    bar: {GET:true},
+    qaz: {GET:true,HEAD:true}
+  }
+}})
+
+seneca.listen()
+
+
+var connect = require('connect')
+var app = connect()
+app.use( connect.query() )
+app.use( seneca.export('web') )
+app.listen(3000)
+
+// run: node test/example.js --seneca.log=type:act
+// try http://localhost:3000/foo/bar?zoo=a
+// returns {"bar":"ab"}
